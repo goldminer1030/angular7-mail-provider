c2492f16e4d2d528f6aae47f808caa8782e8eaa1
goldminer1030 <goldminer1030@gmail.com>
2018-11-29 09:07:17 +0800

    Added Aweber connection

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require('express'),
     bodyParser = require('body-parser'),
     cors = require('cors'),
     passport = require('passport'),
+    session = require('express-session');
     passportMailchimp = require('passport-mailchimp'),
+    passportAweber = require('passport-aweber'),
     MailChimpStrategy = passportMailchimp.Strategy,
+    AweberStrategy = passportAweber.Strategy,
     mongoose = require('mongoose'),
     config = require('./api/DB');
 
@@ -16,24 +19,44 @@ mongoose.connect(config.DB, { useNewUrlParser: true }).then(
 
 const app = express();
 app.use(bodyParser.json()); // support json encoded bodies
-app.use(express.static(__dirname));
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+}));
 app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(__dirname));
 app.use(cors({origin: 'http://127.0.0.1:4000'}));
 
-var querystring = require('querystring');
 var mailchimpInstance   = 'xxxx',
     listUniqueId        = 'xxxxx',
     mailchimpApiKey     = 'xxxxxxxxxx',
     mailchimpClientId   = 'xxxxxxxxxxxxx';
-    mailchimpSecretKey  = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+    mailchimpSecretKey  = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    aweberConsumerKey   = 'xxxxxxxxxxxxxxxxxxxxxxxx',
+    aweberConsumerSecret= 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
 
+// Mailchimp
 passport.use(new MailChimpStrategy({
     clientID: mailchimpClientId,
     clientSecret: mailchimpSecretKey,
-    callbackURL: "/mailchimp/callback"
+    callbackURL: "/mailchimp/callback",
+    passReqToCallback: true
     }, function (accessToken, refreshToken, profile, done) {
         console.log('DONE!!!');
     }
+));
+
+// AWeber
+passport.use(new AweberStrategy({
+    consumerKey: aweberConsumerKey,
+    consumerSecret: aweberConsumerSecret,
+    callbackURL: "/aweber/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    console.log('DONE!!!');
+  }
 ));
 
 // the "index" route, which serves the Angular app
@@ -41,8 +64,8 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname,'/dist/angular7crud/index.html'))
 });
 
+// Mailchimp
 app.get('/mailchimp/authorize', passport.authenticate('mailchimp'));
-
 app.get('/mailchimp/callback',
   passport.authenticate('mailchimp', { failureRedirect: '/mailchimp' }),
   function(req, res) {
@@ -50,6 +73,15 @@ app.get('/mailchimp/callback',
     console.log(res);
     res.redirect('/mailchimp');
 });
+
+// Aweber
+app.get('/aweber/authorize', passport.authenticate('aweber'));
+app.get('/aweber/callback', 
+  passport.authenticate('aweber', { failureRedirect: '/aweber' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/aweber');
+  });
 
 const port = process.env.PORT || 4000;
 
